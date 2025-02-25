ea3a67aa0bedd0f22815ece43b678c959420ba72
GitJenCerio <cerio.dev@gmail.com>
2025-02-25 21:42:19 +0800

    Enhancing the Connectly API with Validation and Relationships

--- connectly_project/urls.py
+++ connectly_project/urls.py
@@ -1,39 +1,14 @@
-"""
-URL configuration for connectly_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
 from django.contrib import admin
 from django.urls import path, include
-from posts import views 
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Welcome to Connectly API!")
 
+# Include 'posts' app URLs
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('admin/', admin.site.urls),  # Admin route
     path('api-auth/', include('rest_framework.urls')),  # DRF login/logout
-    path('posts/', include('posts.urls')),
-        path('users/', views.get_users, name='get_users'),
-    path('users/create/', views.create_user, name='create_user'),
-    path('posts/', views.get_posts, name='get_posts'),
-    path('posts/create/', views.create_post, name='create_post'),
-    path('users/delete/', views.delete_all_users, name='delete_all_users'),
+    path('posts/', include('posts.urls')),  # Routes for the posts app
 ]
-
